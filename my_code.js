884f29b911a9db47203fb60b3d6f87c59ebc858f
David <jollyneuron@gmail.com>
2013-10-21 00:08:07 +0200

    und weiterer commit

--- my_code.js
+++ my_code.js
@@ -1,20 +1,32 @@
  $(document).ready(function() {
-
-
+    
      // bind form using ajaxForm (powered by jQuery form plugin)
      $('#myForm').ajaxForm({ 
 	 success: showResponse, //when the php form returns a success, call showResponse function
 		resetForm: true //after submitting the form, reset it
      }); 
 		
-     //response function for newly created bubble
+     //response function 
      function showResponse(responseText, statusText, xhr, $form) { 
 	
-	 response = parseInt(responseText);
-	 factor = 2;
-	 computation = response * factor;
+	 var response = parseInt(responseText);
+	 base = 2;
+	 var remainder = new Array();
+	 	 
+	 var i = 1;
 
-	 alert(responseText +" x "+ factor + " = " +  computation);
+	 while (response>0)
+	 {
+	 
+	     remainder[i]  = response%base;
+	     response = Math.floor(response/base);
+	     i++;
+
+	    // alert(response);      
+	 } 
+ 
+	 lala = remainder.toString();
+	 alert(lala);
 
      }
 	
