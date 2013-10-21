5900f04512644068cf593183c5907b750e7a57f5
David <jollyneuron@gmail.com>
2013-10-21 23:45:19 +0200

    code läuft

--- my_code.js
+++ my_code.js
@@ -6,27 +6,36 @@
 		resetForm: true //after submitting the form, reset it
      }); 
 		
-     //response function 
+     //response function of echo.php
      function showResponse(responseText, statusText, xhr, $form) { 
 	
 	 var response = parseInt(responseText);
-	 base = 2;
+	 var base = 2;
 	 var remainder = new Array();
-	 	 
-	 var i = 1;
+	 var i = 0;
 
+	 //Do division and store remainder in array
 	 while (response>0)
 	 {
-	 
 	     remainder[i]  = response%base;
 	     response = Math.floor(response/base);
 	     i++;
+         } 
+ 	 
+	 //change direction of array
+	 var result = new Array();
+	 var j = remainder.length;
+	 var k = 0;
 
-	    // alert(response);      
-	 } 
- 
-	 lala = remainder.toString();
-	 alert(lala);
+	 while(j--)
+	 {
+	     result[k] = remainder[j];
+	     k++;
+	 }
+
+	 //give out final result
+	 result_str = result.toString();
+	 alert(result_str);
 
      }
 	
