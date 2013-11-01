9472094586f8fd7b28fce9e06d1e11028a245d71
David <jollyneuron@gmail.com>
2013-11-01 12:33:37 +0100

    umbau: jetzt ohne php

--- my_code.js
+++ my_code.js
@@ -1,29 +1,72 @@
  $(document).ready(function() {
     
-     // bind form using ajaxForm (powered by jQuery form plugin)
-     $('#myForm').ajaxForm({ 
-	 success: showResponse, //when the php form returns a success, call showResponse function
-		resetForm: false //after submitting the form, reset it
-     }); 
-		
+     
+     var direction = 0;
+     var response = 0;
+     
+     $("#inputdec").on('input', function() {
+
+	 direction = 1;
+	 response = $("#inputdec").val();
+	 conversion();
+     });
+
+
      $("#inputbin").on('input', function() {
 
 	 $(this).submit();
-});
+	 direction = 2;
+     });
 
+         
 
-     //response function of echo.php
-     function showResponse(responseText, statusText, xhr, $form) { 
-	
-	 var direction = $("input[name=direction]:checked").val();
-	 var response = responseText;
+     
+     function conversion() {
+	 
+	 //var response = responseText;
 	 var remainder = new Array();
 	 var finalresult = "";
 	 var i = 0;
 
 	 var base = 2;
+	 
+ 	 //Decimal to binary
+	 if (direction==1)
+	 {    
+	 
+	     
+	 
+	     //Do division and store remainder in array
+	     var responseint = parseInt(response);
+	     while (responseint>0)
+	     {
+		 
+		 remainder[i]  = responseint%base;
+		 responseint = Math.floor(responseint/base);
+		 i++;
+		 		 
+             } 
+ 	     
+	     //change direction of array
+	     var result = new Array();
+	     var j = remainder.length;
+	     var k = 0;
 
- 	 //Binary to Decimal
+	     while(j--)
+	     {
+		 result[k] = remainder[j];
+		 k++;
+	     }
+	     
+	     result_str = result.toString();
+	     var result_str2 = result_str.replace(/,/g,""); 
+	     finalresult = result_str2;
+
+	     $("#inputbin").val(finalresult);
+
+	 }    
+
+	 //Binary to Decimal
 	 if (direction==2)
 	 {
 	     var responsetoarray = response.split("");
@@ -43,45 +86,13 @@
 	     }
 	     
 	     finalresult = sumresult;
-	    
-	 }
-	 if (direction==1)
-	 {    
 	 
-	     //Decimal to binary
-	 
-	     //Do division and store remainder in array
-	     var responseint = parseInt(response);
-	     while (responseint>0)
-	     {
-		 remainder[i]  = responseint%base;
-		 responseint = Math.floor(responseint/base);
-		 //alert(response%base);
-		 i++;
-		 
-             } 
- 	     
-	     //change direction of array
-	     var result = new Array();
-	     var j = remainder.length;
-	     var k = 0;
-
-	     while(j--)
-	     {
-		 result[k] = remainder[j];
-		 k++;
-	     }
+	     $("#inputdec").val(finalresult);
 	     
-	     result_str = result.toString();
-	     var result_str2 = result_str.replace(/,/g,""); 
-	     finalresult = result_str2;
-
 	 }
+
 	 
-	 //give out final result
-	 $('#result').html('');
-	 $("#result").hide().append(finalresult).fadeIn(200);
-	 $("#inputdec").val(finalresult);
+	 
 
      }
 	
